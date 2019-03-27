500581420c89a05037d3460fb43a21ba91b56282
sonjmo <shoncmjm@gmail.com>
2019-03-27 11:47:52 +0900

    local change

--- test_java_project/src/test_java_project/test_class.java
+++ test_java_project/src/test_java_project/test_class.java
@@ -3,10 +3,7 @@ package test_java_project;
 public class test_class {
 	public static void main(String[] args) {
 		System.out.println("hello world~! hotfix!");
-<<<<<<< HEAD
 		System.out.println("i'm the master!");
-=======
 		System.out.println("i'm the hotfix~!");
->>>>>>> refs/heads/hotfix
 	}
 }
